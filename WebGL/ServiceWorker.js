84a3db3947de360f1267abd37210f694997ba860
acakebread <acakebread@gmail.com>
2026-08-26 20:26:30 +0100

    Revert "Publish WebGL 0.1.12"
    This reverts commit 49451e5c8e63de54a48b50342600679b4baba27a.

---
+++ WebGL/ServiceWorker.js
@@ -0,0 +1,65 @@
+const cacheName = "MassiveHadron-TileStormEvolution-0.1.11";
+const contentToCache = [
+    "Build/b00be0b2973e4eee3ffc636dd949a21d.loader.js",
+    "Build/6b58587eaab5c67aa27ac9605cf1b480.framework.js.unityweb",
+    "Build/5184aca840fbe8b7df6479c1b1bf0d37.data.unityweb",
+    "Build/4426421e572ddac8ed73174cdd2d2f9c.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+const cacheablePrefixes = [
+    `${self.location.origin}/Build/`,
+    `${self.location.origin}/TemplateData/`
+];
+
+function shouldCache(request) {
+    if (!request || request.method !== "GET")
+        return false;
+
+    if (request.url.startsWith("blob:") || request.url.startsWith("data:"))
+        return false;
+
+    return cacheablePrefixes.some(prefix => request.url.startsWith(prefix));
+}
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil((async function () {
+        const keys = await caches.keys();
+        await Promise.all(keys.map(async key => {
+            if (key !== cacheName)
+                await caches.delete(key);
+        }));
+        await self.clients.claim();
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    if (!shouldCache(e.request))
+        return;
+
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      if (!response || !response.ok)
+        return response;
+
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
