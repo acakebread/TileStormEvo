be88965c756fa6874c8cecf4ece71e18b78884ce
acakebread <acakebread@gmail.com>
2026-09-11 21:41:45 +0100

    Publish WebGL 0.1.31

--- WebGL/ServiceWorker.js
+++ WebGL/ServiceWorker.js
@@ -1,8 +1,8 @@
-const cacheName = "MassiveHadron-TileStormEvolution-0.1.30";
+const cacheName = "MassiveHadron-TileStormEvolution-0.1.31";
 const contentToCache = [
     "Build/8a456944db361d9b22d653600e772a95.loader.js",
     "Build/746ca0375293afc63149f372a6468a54.framework.js.unityweb",
-    "Build/2303a6c9b852e66696357a3ea2cbd284.data.unityweb",
+    "Build/cc5d6065d9d1ec2bcec01033e9f56efd.data.unityweb",
     "Build/fd9065454f836bcdc9b3d4d46054bd3c.wasm.unityweb",
     "TemplateData/style.css"
 
